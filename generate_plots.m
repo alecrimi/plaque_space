9c6796e42bf753e224ca6f7357d29961e2b0fabe
Alessandro Crimi <alecrimi@alice.it>
2019-12-30 19:25:37 +0100

    Update generate_plots.m

--- generate_plots.m
+++ generate_plots.m
@@ -1,27 +1,38 @@
 
-folders_list={'AB-old','AB-young','LCP-old','LCP-young','BACE1-old','BACE1-young'}; %
+folders_list={'AB_old','AB_young','LCP_old','LCP_young','BACE1_old','BACE1_young'}; %
 
 %list = dir([folder '/*.tif']);
 
-feature_name = 'sizes' ;% 'intensities'; %  
+%Change this to change the used features
+feature_name = 'sizes' ;   
+%feature_name = 'intensities'; 
 
-datapoints =  zeros(29*2, 5250  );     %58464  69564  5250
+%This should be defined automatically, but in the data produced by  Ehsan Dadgar-Kiani, the max projection was always giving data of this size 
+datasize_maxproj = 5250; 
+datasize = 58464;  
+datapoints =  zeros(29*2, datasize_maxproj  );     %58464  69564  5250
 
 %sorting = {'default'};
+%sorting = {'default','max_proj'};
 sorting = {'max_proj'};
+
+disp('Remember the stochastic nature of T-SNE, run the algorithm several times to see different results');
+tsne_repetitions = 5;
+perplexity_values = [ 5 10];        
 
 %For all sorting
 for ss = 1 : length(sorting)
-     alg = sorting{ss};
-%{
-    if( strcmp(alg,'max_proj'))
-       datapoints =  zeros(length(list),  5250 );     %58464  69564
-    else
-       datapoints =  zeros(length(list),  58464 );     %58464  69564 
-    end
-%}
 
+     alg = sorting{ss};
+     if( strcmp(alg,'max_proj'))
+       datapoints =  zeros(length(list),  datasize_maxproj );     %58464  69564
+     else
+       datapoints =  zeros(length(list),  datasize );     %58464  69564 
+     end
+
+% Loading data
 %For all channels
+    % In the current version, channel 1 is the template, therefore we should do only channel 2
     for channel = 2 : 2
     count = 1;
       for ll = 1 : length(folders_list)
@@ -53,6 +64,7 @@ for ss = 1 : length(sorting)
         
       end
         %{
+        % PCA part, computed in different ways. For the moment put aside as it was not performing well
         %Center mean
         %Compute cov matrix
         covmat = cov(datapoints);
@@ -76,34 +88,59 @@ for ss = 1 : length(sorting)
         saveas(gcf,['PCA_val_'  folder '_ch' num2str(channel) '_sorting' alg '.png'])
       %}
    
-         score = tsne(datapoints,'Algorithm','exact','Distance','euclidean','Perplexity',10);
-        score = score';
-        figure
-        hold on
-        plot(score(1,1:3),score(2,1:3),'b*')
- %       plot(score(1,4:9),score(2,4:9),'g*')
-        plot(score(1,10:13),score(2,10:13),'r*')
- %       plot(score(1,14:19),score(2,14:19),'c*')
-       plot(score(1,20:24),score(2,20:24),'k*')
- %       plot(score(1,25:27),score(2,25:27),'m*')
+        % T-sne
         
-       plot(score(1,28:33),score(2,28:33),'bo')
- %       plot(score(1,34:37),score(2,34:37),'go')
-        plot(score(1,38:41),score(2,38:41),'ro')
- %      plot(score(1,42:47),score(2,42:47),'co')
-       plot(score(1,48:51),score(2,48:51),'ko')
-%        plot(score(1,51:55),score(2,51:55),'mo')
-%           lgd = legend(folders_list{2},folders_list{4},folders_list{6} );
+        % For all perplexity values
+        for hh = 1 : length(perplexity_values)
         
-        hold off
-        box on
-        xlabel('First component')
-        ylabel('Second component')
-      %  lgd = legend(folders_list{1},folders_list{2},folders_list{3},folders_list{4},folders_list{5},folders_list{6});
-            lgd = legend(folders_list{1},folders_list{3},folders_list{5}, [folders_list{1} 'C'],[folders_list{3} 'C'] ,[ folders_list{5} 'C']);
- %         lgd = legend(folders_list{2},folders_list{4},folders_list{6}, [folders_list{2} 'C'],[folders_list{4} 'C'],[folders_list{6} 'C']);
+        % For all stochastic repetitions
+        for jj = 1 : tsne_repetitions
+             score = tsne(datapoints,'Algorithm','exact','Distance','euclidean','Perplexity',perplexity_values(hh));
+             score = score';
+ 
+             %Plot OLD groups
+             % Assure that order matches the list provided at the begining of the script
+             % Improvement: set those numbers automatically!
+             figure
+             hold on
+             plot(score(1,1:3),score(2,1:3),'b*')
+             plot(score(1,10:13),score(2,10:13),'r*')
+             plot(score(1,20:24),score(2,20:24),'k*')
+             plot(score(1,28:33),score(2,28:33),'bo')
+             plot(score(1,38:41),score(2,38:41),'ro')
+             plot(score(1,48:51),score(2,48:51),'ko')
+             hold off
+             box on
+             xlabel('First component')
+             ylabel('Second component')
+             %Be careful that this order is consistent with the folder list provided at the beginning of the script
+             lgd = legend(folders_list{1},folders_list{3},folders_list{5}, [folders_list{1} 'C'],[folders_list{3} 'C'] ,[ folders_list{5} 'C']);
+             saveas(gcf,['TNSE_val_mean_ch_old' num2str(channel) '_sorting' alg '.png']);
+             saveas(gcf,['TNSE_val_mean_ch_old' num2str(channel) '_sorting' alg '.eps']);
+              
+             %Plot YOUNG groups
+             % Assure that order matches the list provided at the begining of the script
+             % Improvement: set those numbers automatically! 
+             figure
+             hold on
+             plot(score(1,4:9),score(2,4:9),'g*')
+             plot(score(1,14:19),score(2,14:19),'c*')
+             plot(score(1,25:27),score(2,25:27),'m*')
+             plot(score(1,34:37),score(2,34:37),'go')
+             plot(score(1,42:47),score(2,42:47),'co')
+             plot(score(1,51:55),score(2,51:55),'mo')
+             lgd = legend(folders_list{2},folders_list{4},folders_list{6} );
 
-        saveas(gcf,['TNSE_val_mean_ch' num2str(channel) '_sorting' alg '.png'])
-  
+             hold off
+             box on
+             xlabel('First component')
+             ylabel('Second component')
+             %Be careful that this order is consistent with the folder list provided at the beginning of the script
+             lgd = legend(folders_list{2},folders_list{4},folders_list{6}, [folders_list{2} 'C'],[folders_list{4} 'C'],[folders_list{6} 'C']);
+             saveas(gcf,['TNSE_val_mean_ch_old' num2str(channel) '_sorting' alg 'perp_' num2str(perplexity_values(hh)) 'rep_' num2str(jj) '.png']);
+             saveas(gcf,['TNSE_val_mean_ch_old' num2str(channel) '_sorting' alg 'perp_' num2str(perplexity_values(hh)) 'rep_' num2str(jj) '.eps']);
+            
+            end
+       end
     end
 end
